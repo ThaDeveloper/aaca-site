f6436577af7ed3aab593df8977fe2f1123f6aa27
Justin Ndwiga <jndwiga@SamaDc.corp>
2017-05-05 10:06:27 +0300

    Fixed reponsivenes for medium devices

--- index.php
+++ index.php
@@ -129,22 +129,22 @@
          <h2 class="wow fadeInUp">Services</h2>
          <p class="wow fadeInUp" data-wow-delay="0.4">We are a firm that specializes on sports law and provides general legal services that includes but not limited to:</p>
          <div class="row">
-             <div class="col-md-3 wow fadeInLeft" data-wow-delay="1.8s">
+             <div class="col-sm-3 wow fadeInLeft" data-wow-delay="1.8s">
                  <i class="fa fa-gavel" aria-hidden="true"></i>
                  <h4>Legal Advice</h4>
                  <p>For any corporate legal advice.</p>
              </div>
-             <div class="col-md-3 wow fadeInLeft" data-wow-delay="1.4s">
+             <div class="col-sm-3 wow fadeInLeft" data-wow-delay="1.4s">
                  <i class="fa fa-briefcase" aria-hidden="true"></i>
                  <h4>Consultancy</h4>
                  <p>We are with you in your business journey</p>
              </div>
-             <div class="col-md-3 wow fadeInLeft" data-wow-delay="0.8s">
+             <div class="col-sm-3 wow fadeInLeft" data-wow-delay="0.8s">
                  <i class="fa fa-hourglass-half" aria-hidden="true"></i>
                   <h4>Litigation</h4>
                  <p>Lorem Ipsum passages, and more recently with desktop publishing software</p>
              </div>
-             <div class="col-md-3 wow fadeInLeft" data-wow-delay="0.4s">
+             <div class="col-sm-3 wow fadeInLeft" data-wow-delay="0.4s">
                  <i class="fa fa-home" aria-hidden="true"></i>
                  <h4>Conveyancing</h4>
                  <p>Lorem Ipsum passages, and more recently with desktop publishing software</p>
@@ -165,14 +165,14 @@
             <p class="wow fadeInUp" data-wow-delay="0.4">Abdi-Nur & Asiimwe Company Advocates is a firm that specializes on sports law and provides general legal services to a wide range of clients across Kenya.
             Our specialists will work in a very competitive environment and will maneuver around regulatory regimes to get our clients the best deals. We have experience in hard ball negotiations and we work to negotiate in high stake sports related transactions to deliver value for our clients.</p>
             </div>
-            <div class="col-lg-6 col-md-6 wow flipInY" data-wow-delay="0.8s">
+            <div class="col-md-6 col-sm-6 wow flipInY" data-wow-delay="0.8s">
                 <div class="goal">
                 <h4>Our Vision</h4>
                     <p>To provide client-oriented based legal solution services that help them to ensure growth and high profit ends for their business by minimising disputes or settling conflict amicably and quickly whenever they arise without hurting their business environment. 
                     </p>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6 wow flipInY" data-wow-delay="1.2s">
+            <div class="col-md-6 col-sm-6 wow flipInY" data-wow-delay="1.2s">
                 <div class="goal">
                 <h4>Our Mission</h4>
                 <p>
@@ -195,7 +195,7 @@
         <div class="row">
             <h2 class="wow fadeInUp">Meet our team</h2>
             <p class="wow fadeInUp" data-wow-delay="0.4">Lorem Ipsum passages, and more recently with desktop publishing software</p>
-            <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="2s">
+            <div class="col-md-3 col-sm-3 wow fadeInLeft" data-wow-delay="2s">
                 <img src="images/team/team-1.jpg" class="img-circle" alt="">
                 <h4>John Doe</h4>
                 <b>CEO and Founder</b>
@@ -205,7 +205,7 @@
                 <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                 <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
             </div>
-            <div class="col-lg-3 col-md-3  wow fadeInLeft"  data-wow-delay="1.6s">
+            <div class="col-md-3 col-sm-3  wow fadeInLeft"  data-wow-delay="1.6s">
                 <img src="images/team/team-2.jpg" class="img-circle" alt="">
                 <h4>John Doe</h4>
                 <b>CEO and Founder</b>
@@ -215,7 +215,7 @@
                 <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                 <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
             </div>
-            <div class="col-lg-3 col-md-3  wow fadeInLeft"  data-wow-delay="1.2s">
+            <div class="col-md-3 col-sm-3  wow fadeInLeft"  data-wow-delay="1.2s">
                 <img src="images/team/team-3.jpg" class="img-circle" alt="">
                 <h4>John Doe</h4>
                 <b>CEO and Founder</b>
@@ -225,7 +225,7 @@
                 <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                 <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
             </div>
-            <div class="col-lg-3 col-md-3  wow fadeInLeft" 0.8>
+            <div class="col-md-3 col-sm-3  wow fadeInLeft" 0.8>
                 <img src="images/team/team-4.jpg" class="img-circle" alt="">
                 <h4>John Doe</h4>
                 <b>CEO and Founder</b>
@@ -249,16 +249,16 @@
         <div class="row">
             <h2 class="wow fadeInUp">Trusted by</h2>
             <p class="wow fadeInUp" data-wow-delay="0.4">Below are some of our happy clients.</p>
-            <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="2s">
+            <div class="col-md-3 col-sm-3 wow fadeInLeft" data-wow-delay="2s">
                 <li><img src="images/clients/client1.jpg" alt=""></li>
             </div>
-            <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.6s">
+            <div class="col-md-3 col-sm-3 wow fadeInLeft" data-wow-delay="1.6s">
                 <li><img src="images/clients/client2.jpg" alt=""></li>
             </div>
-            <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.2s">
+            <div class="col-md-3 col-sm-3 wow fadeInLeft" data-wow-delay="1.2s">
                 <li><img src="images/clients/client3.jpg" alt=""></li>
             </div>
-            <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="0.8s">
+            <div class="col-md-3 col-sm-3 wow fadeInLeft" data-wow-delay="0.8s">
                 <li><img src="images/clients/client4.jpg" alt=""></li>
             </div>
         </div>
@@ -275,7 +275,7 @@
             <h2 class="wow fadeInUp">Contact</h2>
             <p class="wow fadeInUp" data-wow-delay="0.4">Send us a message, or contact us from the address below</p>
             <form id="contactus" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" class="form-horizontal">
-            <div class="col-lg-6 col-md-6">
+            <div class="col-md-6 col-sm-6">
                 <div class="input-group input-group-lg wow fadeInUp" data-wow-delay="0.8s">
                     <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
                     <input type="text" name="name" class="form-control" aria-describedby="sizing-addon1" value="<?= $name ?>" placeholder="Full Name">
@@ -292,7 +292,7 @@
                     <span class="error"><?= $phone_error ?></span>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6">
+            <div class="col-md-6 col-sm-6">
                 <div class="input-group wow fadeInUp" data-wow-delay="2s">
                     <textarea value="<?= $message ?>" name="message" id="message" cols="80" rows="6" class="form-control"></textarea>
                 </div>
@@ -313,21 +313,21 @@
  <div id="footer" class="footer">
      <div class="container">
          <div class="row">
-             <div class="col-lg-4 col-md-4">
+             <div class="col-md-4 col-sm-4">
                  <h4 class="wow fadeInUp">Contact Us</h4>
                  <p><i class="fa fa-home" aria-hidden="true"></i>Bruce House, 3rd Floor, Nairobi CBD</p>
                  <p><i class="fa fa-envelope" aria-hidden="true"></i>info@abdinurasiimwe.ac.ke</p>
                  <p><i class="fa fa-phone" aria-hidden="true"></i> +25422569583 or +256 7005620162</p>
                  <p><i class="fa fa-globe" aria-hidden="true"></i> abdinurasiimwe.ac.ke </p>
              </div>
-             <div class="col-lg-4 col-md-4">
+             <div class="col-md-4 col-sm-4">
                  <h4>About</h4>
                  <p><i class="fa fa-square-o" aria-hidden="true"></i> About Us</p>
                  <p><i class="fa fa-square-o" aria-hidden="true"></i> Privacy</p>
                  <p><i class="fa fa-square-o" aria-hidden="true"></i> Term & Condition</p>
                  <p>&copy; Copyright @ 2107</p>
              </div>
-             <div class="col-lg-4 col-md-4">
+             <div class="col-md-4 col-sm-4">
                  <h4>Stay in touch</h4>
                  <i class="social fa fa-facebook" aria-hidden="true"></i>
                  <i class="social fa fa-twitter" aria-hidden="true"></i>
@@ -335,7 +335,7 @@
                 <i class="social fa fa-pinterest" aria-hidden="true"></i>
                 <i class="social fa fa-youtube" aria-hidden="true"></i>
                 <i class="social fa fa-github" aria-hidden="true"></i><br>
-                <input type="email" placeholder="Subscribe For Updates"><button class="btn btn-success">Subscribe</button>
+               <!-- <input type="email" placeholder="Subscribe For Updates"><button class="btn btn-success">Subscribe</button> -->
              </div>
          </div>
         
